695c061308fdfd6f9585cb0c989d9578a4af2cd3
shawn7797 <shawn7797@gmail.com>
2022-07-01 14:39:17 +0530

    Added static

--- mysite/urls.py
+++ mysite/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from django.conf import settings
+from django.conf.urls.static import static
 
 admin.site.site_title = "Shawn's Admin Page"
 admin.site.site_header = "Shawn's Admin Page"
@@ -22,4 +24,4 @@ admin.site.site_header = "Shawn's Admin Page"
 urlpatterns = [
     path('app/', include('myapp.urls')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
