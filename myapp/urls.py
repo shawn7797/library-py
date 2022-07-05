cf8d22048f8885414012cd4984d0e9bff7337b07
shawn7797 <shawn7797@gmail.com>
2022-07-05 23:34:39 +0530

    Fixed base path

--- myapp/urls.py
+++ myapp/urls.py
@@ -5,7 +5,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    # path("", views.index, name="index"),
+    path("", views.index, name="index"),
     path("get_book", views.get_book, name="get_book"),
     path("get_book_by_id/<int:book_id>", views.get_book_by_id, name="get_book_by_id"),
     path("get_books", views.get_books, name="get_all_books"),
